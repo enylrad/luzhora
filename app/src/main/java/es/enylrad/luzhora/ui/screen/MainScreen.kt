b4e6333e08e4fdd7e44b6689beeafe2fe2327710
David Pérez Salort <enylrad@gmail.com>
2021-07-10 15:43:48 +0200

    Update libraries New implementation lottie

--- app/src/main/java/es/enylrad/luzhora/ui/screen/MainScreen.kt
+++ app/src/main/java/es/enylrad/luzhora/ui/screen/MainScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -12,9 +13,7 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieAnimationSpec
-import com.airbnb.lottie.compose.rememberLottieAnimationState
+import com.airbnb.lottie.compose.*
 import es.enylrad.luzhora.R
 import es.enylrad.luzhora.data.model.Price
 import es.enylrad.luzhora.ui.theme.LuzhoraTheme
@@ -34,22 +33,35 @@ fun MainBody(price: Price) {
                 text = stringResource(price.type),
                 style = TextStyle(fontSize = 25.sp)
             )
-
-            val animationSpec = LottieAnimationSpec.RawRes(price.lottieAnim)
-            val animationState = rememberLottieAnimationState(
-                autoPlay = true,
-                repeatCount = Integer.MAX_VALUE
-            )
-            LottieAnimation(
-                spec = animationSpec,
-                modifier = Modifier.size(300.dp),
-                animationState = animationState
-            )
+            AnimationPrice(price)
             Text(
                 text = stringResource(id = R.string.animated_by_s, "Waalled Hasmi"),
                 style = TextStyle(fontSize = 12.sp, fontStyle = FontStyle.Italic)
             )
         }
+    }
+}
+
+@Composable
+private fun AnimationPrice(price: Price) {
+
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(price.lottieAnim))
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+    LottieAnimation(
+        composition = composition,
+        modifier = Modifier.size(300.dp),
+        progress = progress,
+    )
+}
+
+@Preview
+@Composable
+fun AnimationPricePreview() {
+    LuzhoraTheme {
+        AnimationPrice(Price.Low())
     }
 }
 
